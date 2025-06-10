3b152b0736d977f0f6a0b599fbb18d6c5d2352f3
howard19681223 <howard681223@gmail.com>
2025-06-10 16:56:45 +0800

    Update MainActivity.java

--- MyNDK01/app/src/main/java/com/example/ndkdemo/MainActivity.java
+++ MyNDK01/app/src/main/java/com/example/ndkdemo/MainActivity.java
@@ -1,1 +1,33 @@
+package com.example.ndkdemo;
+
+import android.os.Bundle;
+import android.widget.TextView;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+    static {
+        System.loadLibrary("native-lib");
+    }
+
+    // JNI 方法声明
+    public native int intFromJNI(int input);
+    public native String stringFromJNI(String input);
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        int inputInt = 42;
+        String inputStr = "Hello NDK";
+        int resultInt = intFromJNI(inputInt);
+        String resultStr = stringFromJNI(inputStr);
+
+        TextView tv = new TextView(this);
+        tv.setText(
+            "Java->C int: " + inputInt + " => " + resultInt + "\n" +
+            "Java->C str: " + inputStr + " => " + resultStr
+        );
+        setContentView(tv);
+    }
+}
 
