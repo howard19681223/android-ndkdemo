84b7ca36545ad850477188dbb4e12d8c785bf149
howard19681223 <howard681223@gmail.com>
2025-06-10 17:29:47 +0800

    Update native-lib.c

--- MyNDK01/app/src/main/cpp/native-lib.c
+++ MyNDK01/app/src/main/cpp/native-lib.c
@@ -1,1 +1,20 @@
-ss
+#include <jni.h>
+#include <string.h>
+
+// 传递和返回整数
+JNIEXPORT jint JNICALL
+Java_com_example_ndkdemo_MainActivity_intFromJNI(JNIEnv* env, jobject thiz, jint input) {
+    return input * 2; // 简单地乘2返回
+}
+
+// 传递和返回字符串
+JNIEXPORT jstring JNICALL
+Java_com_example_ndkdemo_MainActivity_stringFromJNI(JNIEnv* env, jobject thiz, jstring input) {
+    const char *inC = (*env)->GetStringUTFChars(env, input, 0);
+    char outC[100] = "Echo from C: ";
+    strncat(outC, inC, 80);
+    (*env)->ReleaseStringUTFChars(env, input, inC);
+    return (*env)->NewStringUTF(env, outC);
+}
+
+
